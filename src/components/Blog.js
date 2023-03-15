99d1c164e0f929803d5a649870061c4cdc7018f2
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-03-14 18:38:20 -0700

    exercise 5.7 - feat: first working version of Blog details toggle
    Still missing the user information though

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,10 +1,27 @@
-import { useState, useImperativeHandle, forwardRef } from 'react'
+import { useState } from 'react'
 
 const Blog = ({blog}) => {
   const [showDetail, setShowDetail] = useState(false)
 
   const toggleDetail = () => {
     setShowDetail(!showDetail)
+  }
+
+  console.log(blog)
+
+  if (showDetail) {
+    return (
+      <div className="blog">
+        <div>
+          {blog.title} {blog.author}
+          &nbsp;
+          <button onClick={toggleDetail}>hide details</button>
+        </div>
+        <div>{blog.url}</div>
+        <div>{blog.likes}</div>
+        <div></div>
+      </div>
+    )
   }
 
   return (
