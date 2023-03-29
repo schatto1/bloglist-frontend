eb9d7767928e4b9883d3ecb4b28c20f710518f07
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-03-29 15:06:28 -0700

    exercise 5.15 - test: fix blog URL and likes test

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -30,28 +30,38 @@ test('renders content', () => {
   expect(testAuthor).toBeDefined()
 })
 
-// test('clicking the button shows blog URL and likes', async () => {
-//   const blog = {
-//     title: 'Component testing is done with react-testing-library',
-//     author: 'Full Stack Open Author',
-//     url: 'this-is-an-url',
-//     likes: 30,
-//   }
+test('clicking the button shows blog URL and likes', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Full Stack Open Author',
+    url: 'this-is-an-url',
+    likes: 30,
+  }
 
-//   const mockHandler = jest.fn()
+  const testUser = {
+    username: 'tester',
+    name: 'Testy McTesterson'
+  }
 
-//   render(<Blog blog={blog} handleLike={handleLike} handleRemove={handleRemove} currentUser={user} />)
+  const mockHandler = jest.fn()
 
-//   const testUser = userEvent.setup()
-//   const button = screen.getByText('view')
-//   await testUser.click(button)
+  render(<Blog
+    blog={blog}
+    handleLike={() => console.log('like')}
+    handleRemove={() => console.log('remove')}
+    currentUser={testUser}
+  />)
 
-//   const testUrl = screen.getByText('this-is-an-url')
-//   expect(testUrl).toBeDefined()
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
 
-//   const testLikes = screen.getByText('30')
-//   expect(testLikes).toBeDefined()
-// })
+  const testUrl = screen.getByText('this-is-an-url')
+  expect(testUrl).toBeDefined()
+
+  const testLikes = screen.getByText('30')
+  expect(testLikes).toBeDefined()
+})
 
 // test('clicking the like button increments likes by two', async () => {
 //   const blog = {
