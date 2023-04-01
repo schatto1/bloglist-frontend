c5bf60580d78a2fb042e6138fa5591a521fd5139
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-03-31 22:26:42 -0700

    exercise 5.18 - test: adds user creation via backend in cypress beforeEach

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Testy McTesterson',
+      username: 'testy',
+      password: 'testerson'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
